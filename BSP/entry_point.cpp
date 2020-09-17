8b1eae1d5c7b4930ef5f3f680abae36c40c1200e
Vojtěch Michal <vojta.michall@email.cz>
2020-09-17 04:11:34 +0200

    Configure system clock.

--- BSP/entry_point.cpp
+++ BSP/entry_point.cpp
@@ -7,13 +7,33 @@
  */
 
 #include <Bootloader/main.hpp>
+#include <ufsel/bit_operations.hpp>
 
 #include "stm32f10x.h"
 
 namespace bsp {
 
-	extern "C" void pre_main() {
+	using namespace ufsel;
 
+	//Initializes system clock to max frequencies: 72MHz SYSCLK, AHB, APB2; 32MHz APB1
+	extern "C" void pre_main() {
+		bit::set(std::ref(RCC->CR), RCC_CR_HSEON); //Enable external oscilator
+		while (bit::all_cleared(RCC->CR, RCC_CR_HSERDY)); //wait for it to stabilize
+
+		bit::set(std::ref(RCC->CFGR),
+			RCC_CFGR_PLLMULL9, //Make PLL multiply 8MHz * 9 -> 72 MHz (max frequency)
+			RCC_CFGR_PLLSRC, //clock it from 8 MHz HSE
+			RCC_CFGR_PPRE1_DIV2, //Divide clock for APB1 by 2 (max 36 MHz)
+			RCC_CFGR_ADCPRE_DIV6 //Divide by six for adc (max 14 MHz)
+			);
+
+		bit::modify(std::ref(FLASH->ACR), FLASH_ACR_LATENCY, FLASH_ACR_LATENCY_2); //Flash latency of two wait states
+
+		bit::set(std::ref(RCC->CR), RCC_CR_PLLON); //Enable PLL
+		while (bit::all_cleared(RCC->CR, RCC_CR_PLLRDY)); //wait for it to stabilize
+
+		bit::modify(std::ref(RCC->CFGR), RCC_CFGR_SW_0 | RCC_CFGR_SW_1, RCC_CFGR_SW_PLL); //Set PLL as system clock
+		while (bit::sliceable_value{RCC->CFGR}[bit::slice{1,0}] != RCC_CFGR_SWS_PLL); //wait for it settle
 	}
 
 	extern "C" int main() {
