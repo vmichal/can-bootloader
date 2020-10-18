87b45f0c3f3a8c6c12c81c0819d5d74b9d69bae8
Vojtěch Michal <vojta.michall@email.cz>
2020-10-19 00:16:54 +0200

    Add UFSEL configuration file.

---
+++ ufsel-configuration.hpp
@@ -0,0 +1,21 @@
+/*
+ * eForce CAN Bootloader
+ *
+ * Written by Vojtìch Michal
+ *
+ * Copyright (c) 2020 eforce FEE Prague Formula
+ *
+ * This header file contains configuration of the UFSEL library
+ */
+#pragma once
+
+#include <cstdint>
+
+namespace ufsel {
+
+
+	namespace bit {
+		using machine_word = std::uint32_t;
+	}
+
+}
