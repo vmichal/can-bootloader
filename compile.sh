f3aa30ab3b0d22d0199e1bdfe6210ebdbf9c934f
Vojtěch Michal <vojta.michall@email.cz>
2021-11-12 01:39:26 +0100

    Allow building all bootloader versions with './compile.sh all'

--- compile.sh
+++ compile.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-mkdir -p build
-mkdir -p build/STM32$1
+if [ $1 == 'all' ] || [ $1 == 'ALL' ]
+then
+  ./compile.sh F1
+  ./compile.sh F2
+  ./compile.sh F4
+  ./compile.sh F7
+else
+  mkdir -p build
+  mkdir -p build/STM32$1
 
-cd build/STM32$1
-cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=stm32$1.cmake ../..
-ninja
+  cd build/STM32$1
+  cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=stm32$1.cmake ../..
+  ninja
+fi
+
 
