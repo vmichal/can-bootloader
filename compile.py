0efc93696f358e226df11fcfcf9a3229de563c5c
Vojtěch Michal <vojta.michall@email.cz>
2023-07-06 21:59:12 +0200

    Correct allocation of front and rear MCs.

--- compile.py
+++ compile.py
@@ -22,8 +22,8 @@ build_data = [
 	('FSE12', 'FSB', 'f4', 12, 1000, ('B', 8), ('B', 9), ('B', 5), ('B', 6)),
 	('FSE12', 'STW', 'f7', 12, 1000, ('A', 11), ('A', 12), ('B', 12), ('B', 13)),
 	# TODO decide which disruptor will be front and rear
-	('FSE12', 'DRTF', 'g4', 12, 1000, ('B', 8), ('B', 9), ('B', 5), ('B', 6)), # corresponds to Disruptor V1
-	('FSE12', 'DRTR', 'g4', 12, 1000, ('D', 0), ('D', 1), ('B', 12), ('B', 13)), # corresponds to Disruptor V1_1
+	('FSE12', 'DRTR', 'g4', 12, 1000, ('B', 8), ('B', 9), ('B', 5), ('B', 6)), # corresponds to Disruptor V1
+	('FSE12', 'DRTF', 'g4', 12, 1000, ('D', 0), ('D', 1), ('B', 5), ('B', 6)), # corresponds to Disruptor V1_1
 ]
 
 for data in build_data:
